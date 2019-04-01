ddaa1fefd763f4eca3f8ead4be7aa20444be35c5
DAB0mB <emanor6@gmail.com>
2019-04-01 21:24:54 +0800

    step-by-step-final@next: Initial version of the full tutorial

---
+++ index.ts
@@ -0,0 +1,41 @@
+import { ApolloServer, gql, PubSub } from 'apollo-server-express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import express from 'express'
+import http from 'http'
+import { users } from './db'
+import schema from './schema'
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+app.get('/_ping', (req, res) => {
+  res.send('pong')
+})
+
+const pubsub = new PubSub()
+const server = new ApolloServer({
+  schema,
+  context: ({ req }) => ({
+    currentUser: users.find(u => u.id === req.cookies.currentUserId),
+    pubsub,
+  }),
+})
+
+const origin = process.env.ORIGIN || 'http://localhost:3000'
+server.applyMiddleware({
+  app,
+  path: '/graphql',
+  cors: { credentials: true, origin },
+})
+
+const httpServer = http.createServer(app)
+server.installSubscriptionHandlers(httpServer)
+
+const port = process.env.PORT || 4000
+
+httpServer.listen(port, () => {
+  console.log(`Server is listening on port ${port}`)
+})
